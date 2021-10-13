22abe90906ff6f669443426fd5811ac716ae90e1
Robertkochbr <robert.koch@novaalianca.coop.br>
2021-10-13 09:34:03 -0300

    GLPI 11065 Melhorias VA_VSTATUS_CUSTO_MEDIO

--- View VA_VSTATUS_CUSTO_MEDIO.sql
+++ View VA_VSTATUS_CUSTO_MEDIO.sql
@@ -16,15 +16,18 @@ GO
 -- Historico de alteracoes:
 -- 11/12/2017 - Robert - Ignora registros com o campo CV8_SBPROC preenchido (sobprocessos).
 -- 14/05/2021 - Robert - Busca MV_ULMES no SX6 pois agora temos no banco de dados.
+-- 13/10/2021 - Robert - Passa a ler SYS_COMPANY e nao mais VA_SM0. Incluidos SBJ, SBK e ultima virada (GLPI 11065).
 --
 
 ALTER VIEW [dbo].[VA_VSTATUS_CUSTO_MEDIO] AS
 
 SELECT SM0.M0_CODIGO AS EMPRESA,
        M0_CODFIL AS FILIAL,
-       ISNULL(FIM.CV8_DATA, '') AS DT_ULT_RECALC,
-       ISNULL(FIM.CV8_HORA, '') AS HR_ULT_RECALC,
-       ISNULL(FIM.CV8_USER, '') AS USER_ULT_RECALC,
+	   (SELECT RTRIM (X6_CONTEUD)
+	   FROM SX6010
+	   WHERE D_E_L_E_T_ = ''
+	   AND X6_FIL = M0_CODFIL
+	   AND X6_VAR = 'MV_ULMES') AS MV_ULMES,
        ISNULL(
            (
                SELECT MAX(B9_DATA)
@@ -34,29 +37,69 @@ SELECT SM0.M0_CODIGO AS EMPRESA,
            ),
            ''
        ) AS ULT_FECHTO_SB9,
-	   (SELECT RTRIM (X6_CONTEUD)
-	   FROM SX6010
-	   WHERE D_E_L_E_T_ = ''
-	   AND X6_FIL = M0_CODFIL
-	   AND X6_VAR = 'MV_ULMES') AS MV_ULMES
-FROM   VA_SM0 SM0
-       LEFT JOIN CV8010 FIM -- ENCONTRA O FIM DA ULTIMA EXECUCAO COMPLETA DO RECALCULO DO CUSTO MEDIO
+       ISNULL(
+           (
+               SELECT MAX(BJ_DATA)
+               FROM   SBJ010 SBJ
+               WHERE  SBJ.D_E_L_E_T_ = ''
+                      AND SBJ.BJ_FILIAL = SM0.M0_CODFIL
+           ),
+           ''
+       ) AS ULT_FECHTO_SBJ,
+       ISNULL(
+           (
+               SELECT MAX(BK_DATA)
+               FROM   SBK010 SBK
+               WHERE  SBK.D_E_L_E_T_ = ''
+                      AND SBK.BK_FILIAL = SM0.M0_CODFIL
+           ),
+           ''
+       ) AS ULT_FECHTO_SBK,
+       ISNULL(RECALC.CV8_DATA, '') AS DT_ULT_RECALC,
+       ISNULL(RECALC.CV8_HORA, '') AS HR_ULT_RECALC,
+       ISNULL(RECALC.CV8_USER, '') AS USER_ULT_RECALC,
+       ISNULL(VIRADA.CV8_DATA, '') AS DT_ULT_VIRADA,
+       ISNULL(VIRADA.CV8_HORA, '') AS HR_ULT_VIRADA,
+       ISNULL(VIRADA.CV8_USER, '') AS USER_ULT_VIRADA
+FROM   SYS_COMPANY SM0
+       LEFT JOIN CV8010 RECALC -- ENCONTRA O FIM DA ULTIMA EXECUCAO COMPLETA DO RECALCULO DO CUSTO MEDIO
             ON  (
-                    FIM.D_E_L_E_T_ = ''
-                    AND FIM.CV8_FILIAL = SM0.M0_CODFIL
-                    AND FIM.CV8_PROC = 'MATA330'
-                    AND FIM.CV8_SBPROC = ''  -- para ignorar subprocessos
-                    AND FIM.CV8_INFO = '2'
-                    AND FIM.CV8_MSG NOT LIKE 'Sub-Processo%'
-                    AND FIM.CV8_DATA + FIM.CV8_HORA + CAST(FIM.R_E_C_N_O_ AS VARCHAR) 
+                    RECALC.D_E_L_E_T_ = ''
+                    AND RECALC.CV8_FILIAL = SM0.M0_CODFIL
+                    AND RECALC.CV8_PROC = 'MATA330'
+                    AND RECALC.CV8_SBPROC = ''  -- para ignorar subprocessos
+                    AND RECALC.CV8_INFO = '2'
+                    AND RECALC.CV8_MSG NOT LIKE 'Sub-Processo%'
+                    AND RECALC.CV8_DATA + RECALC.CV8_HORA + CAST(RECALC.R_E_C_N_O_ AS VARCHAR) 
                         >= (
                             SELECT MAX(
                                        INICIO.CV8_DATA + INICIO.CV8_HORA + CAST(INICIO.R_E_C_N_O_ AS VARCHAR)
                                    )
                             FROM   CV8010 INICIO
                             WHERE  INICIO.D_E_L_E_T_ = ''
-                                   AND INICIO.CV8_FILIAL = FIM.CV8_FILIAL
-                                   AND INICIO.CV8_PROC = FIM.CV8_PROC
+                                   AND INICIO.CV8_FILIAL = RECALC.CV8_FILIAL
+                                   AND INICIO.CV8_PROC = RECALC.CV8_PROC
+                                   AND INICIO.CV8_SBPROC = ''  -- para ignorar subprocessos
+                                   AND INICIO.CV8_INFO = '1'
+                        )
+                )
+       LEFT JOIN CV8010 VIRADA -- ENCONTRA O FIM DA ULTIMA EXECUCAO DA VIRADA DE SALDOS
+            ON  (
+                    VIRADA.D_E_L_E_T_ = ''
+                    AND VIRADA.CV8_FILIAL = SM0.M0_CODFIL
+                    AND VIRADA.CV8_PROC = 'MATA280'
+                    AND VIRADA.CV8_SBPROC = ''  -- para ignorar subprocessos
+                    AND VIRADA.CV8_INFO = '2'
+                    AND VIRADA.CV8_MSG NOT LIKE 'Sub-Processo%'
+                    AND VIRADA.CV8_DATA + VIRADA.CV8_HORA + CAST(VIRADA.R_E_C_N_O_ AS VARCHAR) 
+                        >= (
+                            SELECT MAX(
+                                       INICIO.CV8_DATA + INICIO.CV8_HORA + CAST(INICIO.R_E_C_N_O_ AS VARCHAR)
+                                   )
+                            FROM   CV8010 INICIO
+                            WHERE  INICIO.D_E_L_E_T_ = ''
+                                   AND INICIO.CV8_FILIAL = VIRADA.CV8_FILIAL
+                                   AND INICIO.CV8_PROC = VIRADA.CV8_PROC
                                    AND INICIO.CV8_SBPROC = ''  -- para ignorar subprocessos
                                    AND INICIO.CV8_INFO = '1'
                         )
