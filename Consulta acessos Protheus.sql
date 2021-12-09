07962e8f0570fd1a1bbabe8c398a3a43f3aa14a0
Robertkochbr <robert.koch@novaalianca.coop.br>
2021-12-08 21:15:52 -0300

    Consulta acessos Protheus

--- Consulta acessos Protheus.sql
+++ Consulta acessos Protheus.sql
@@ -15,7 +15,7 @@ WHERE SU.D_E_L_E_T_ = ''
 --AND upper (SGG.GR__NOME) like '%FIS%'
 AND SU.USR_MSBLQL != '1'
 --AND SUG.USR_GRUPO != '000000'
-AND SUG.USR_GRUPO in ('000013', '000056', '000125', '000203')
+AND SUG.USR_GRUPO in ('000066', '000056', '000125', '000203')
 --and USR_CODIGO not like 'cupom%'
 and USR_CODIGO not like 'Administrador%'
 
@@ -46,3 +46,27 @@ WHERE D_E_L_E_T_ = ''
 AND USR_MSBLQL != '1'
 ORDER BY USR_ID
 */
+
+
+/* Andamento da migração de usuários 'por setor' para 'por função'
+WITH C AS (
+SELECT G.GR__ID, G.GR__CODIGO, G.GR__NOME
+	,(SELECT count (*)
+		FROM SYS_USR_GROUPS U
+		WHERE U.D_E_L_E_T_ = ''
+		AND U.USR_GRUPO = G.GR__ID) AS QT_USUARIOS
+FROM SYS_GRP_GROUP G
+WHERE G.D_E_L_E_T_ = ''  -- Nao deletado
+AND G.GR__MSBLQL != '1'  -- Nao bloqueado
+AND G.GR__NOME NOT LIKE 'Administradores'  -- Grupo de gerencialento do sistema
+AND G.GR__NOME NOT LIKE 'Representantes'  -- Representantes nao usam mais o Protheus.
+AND G.GR__CODIGO NOT LIKE 'Geral'  -- Acessos gerais (todos os usuarios fazem parte)
+AND G.GR__CODIGO NOT LIKE 'Filia%'  -- Especifico para determinar as filiais que o usuario vai acessar
+AND G.GR__CODIGO NOT LIKE 'Aprendiz%'  -- Especifico para manores aprendizes
+ORDER BY G.GR__ID
+)
+SELECT SUM (CASE WHEN GR__CODIGO LIKE 'Funcao%' THEN 1 ELSE 0 END) AS QT_USR_MIGRADOS,
+SUM (CASE WHEN GR__CODIGO LIKE 'Funcao%' THEN 0 ELSE 1 END) AS QT_USR_PENDENTES
+FROM C
+*/
+
