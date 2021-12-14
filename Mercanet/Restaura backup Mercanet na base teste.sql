d89385b1e19813288746a952958a00ed001f7cad
Robertkochbr <robert.koch@novaalianca.coop.br>
2021-12-14 09:59:08 -0300

    Migração fontes database Mercanet

---
+++ Mercanet/Restaura backup Mercanet na base teste.sql
@@ -0,0 +1,82 @@
+-- Script para restaurar backup da base quente do Mercanet por cima da base teste
+-- Data: 01/10/2021
+-- Autor: Robert Koch
+--
+-- Historico de alteracoes:
+--
+
+Use master
+GO
+
+
+/* Para acessar o arquivo de backup provavelmente seja necess�rio mapear uma unidade PELO SQL (pelo
+   Windows n�o funciona). Antes disso, se o SQL ainda n�o estiver com a fun��o de shell habilitada,
+   pode ser necess�rio o seguinte :
+-- To allow advanced options to be changed.  
+EXEC sp_configure 'show advanced options', 1;  
+GO  
+-- To update the currently configured value for advanced options.  
+RECONFIGURE;  
+GO  
+-- To enable the feature.  
+EXEC sp_configure 'xp_cmdshell', 1;  
+GO  
+-- To update the currently configured value for this feature.  
+RECONFIGURE;  
+GO  
+*/
+
+/* Exemplos para mapear o drive K: pelo SQL (n�o adianta fazer pelo Windows):
+--exec xp_cmdshell 'net use k: \\192.168.2.94\shared'
+--exec xp_cmdshell 'net use n: /delete'
+--exec xp_cmdshell 'net use k: \\192.168.1.9\bkp-sql /user:admin Alianca164'
+--exec xp_cmdshell 'net use k: \\nas-backup\bkp-sql /user:admin Alianca164'
+--exec xp_cmdshell 'net use N: \\192.168.1.104\bkp-sql /user:bkp.srvsql T4p3Cor3!'
+*/
+
+/*
+-- Verifica conteudo do arquivo (pode haver mais de 1 backup no arquivo). Nesse caso,
+-- usar WITH FILE= no comando de restore para especificar qual backup deve ser restaurado.
+declare @nome_arq_bkp varchar (60) = N'n:\MercanetPRD\MercanetPRD_Full 01-00 30-09-2021.bak'
+--RESTORE HEADERONLY FROM DISK = @nome_arq_bkp
+
+-- Restaura o backup no database novo. Documentacao em https://docs.microsoft.com/pt-br/sql/relational-databases/backup-restore/restore-a-database-to-a-new-location-sql-server?view=sql-server-2017
+use master;
+RESTORE DATABASE [MercanetHML]  -- para onde vai ser restaurado
+FROM DISK = @nome_arq_bkp  -- de onde vai ser restaurado
+WITH FILE = 1,  -- se o arq. de backup tiver mais de um backup, quero o primeiro
+REPLACE,  -- para permitir renomear o database (restaurar para um nome diferente do database onde foi feito o backup)
+MOVE N'MercanetPRD' TO N'c:\Dados_SQL\MercanetHML.mdf',  -- move o nome logico do database original para o arquivo (fisico) destino do restore
+MOVE N'MercanetPRD_log' TO N'c:\Dados_SQL\MercanetHML_log.ldf',  -- move o nome logico do database original para o arquivo (fisico) destino do restore
+NORECOVERY  -- isso por que quero restaurar algum arquivo de log em seguida
+GO
+
+-- Restaura alguns arquivos de backup log
+use master;
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 02-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 03-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 04-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 05-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 06-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 07-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 08-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 09-00.trn' WITH FILE = 1, NORECOVERY
+RESTORE LOG [MercanetHML] FROM DISK = N'n:\MercanetPRD\MercanetPRD_Log 30-09-2021 10-00.trn' WITH FILE = 1
+
+
+-- monitora processo (abrir em janela separada)
+select percent_complete, total_elapsed_time / 60 / 1000 as minutos_executado, estimated_completion_time / 60 / 1000 as minutos_restantes, * from sys.dm_exec_requests where (command = 'RESTORE DATABASE' or command like 'BACKUP%' or command like 'ALTER%' or command = 'DbccFilesCompact')
+
+
+-- Ajusta seguranca e acessos.
+use MercanetHML
+EXEC dbo.sp_changedbowner @loginame = N'mercanet', @map = false
+GO
+
+-- Altera nome logico dos arquivos (database precisa estar offline) e os arquivos devem ser renomeados pelo Windows.
+ALTER DATABASE [MercanetHML] MODIFY FILE (NAME=N'MercanePRD', NEWNAME=N'MercanetHML')
+ALTER DATABASE [MercanetHML] MODIFY FILE (NAME=N'MercanetPRD_log', NEWNAME=N'MercanetHML_log')
+ALTER DATABASE [MercanetHML] SET RECOVERY SIMPLE WITH NO_WAIT
+GO
+
+*/
