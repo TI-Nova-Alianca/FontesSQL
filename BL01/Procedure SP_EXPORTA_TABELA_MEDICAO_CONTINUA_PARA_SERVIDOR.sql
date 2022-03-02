3cb243a755cd6bfc84c8c3ed71aa8245f8b4a5c5
Robertkochbr <robert.koch@novaalianca.coop.br>
2022-03-02 11:53:38 -0300

    BL01 - Atualização procedures grau contínuo

--- BL01/Procedure SP_EXPORTA_TABELA_MEDICAO_CONTINUA_PARA_SERVIDOR.sql
+++ BL01/Procedure SP_EXPORTA_TABELA_MEDICAO_CONTINUA_PARA_SERVIDOR.sql
@@ -1,6 +1,6 @@
 USE [BL01]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_EXPORTA_TABELA_MEDICAO_CONTINUA_PARA_SERVIDOR]    Script Date: 28/02/2022 15:04:31 ******/
+/****** Object:  StoredProcedure [dbo].[SP_EXPORTA_TABELA_MEDICAO_CONTINUA_PARA_SERVIDOR]    Script Date: 02/03/2022 08:59:33 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,24 +13,45 @@ GO
 -- Autor:     Robert Koch
 --
 -- Historico de alteracoes:
+-- 01/03/2022 - Robert - Passa a receber filial e safra por parametro
+--                     - Gera tabelas temporarias de cargas jah presentes no servidor antes
+--                       de fazer o insert (melhora de performance)
 --
 
 ALTER PROCEDURE [dbo].[SP_EXPORTA_TABELA_MEDICAO_CONTINUA_PARA_SERVIDOR]
-AS
+(
+	@filial VARCHAR (2)
+	,@safra VARCHAR (4)
+) AS
 BEGIN
+
+	-- Gera uma tabela temporaria com as medicoes jah presentes no servidor, para posteriormente
+	-- poder verificar se cada medicao deve ou nao ser exportada.
+	drop table if exists #MEDICOES_CONTINUAS_jah_no_servidor
+	select FILIAL, LINHA, HORA
+		into #MEDICOES_CONTINUAS_jah_no_servidor
+		from LKSRV_SERVERSQL_BL01.BL01.dbo.MEDICOES_CONTINUAS T
+		where FILIAL = cast (@filial as int)
+		  and HORA >= @safra
+	create nonclustered index ind1 on #MEDICOES_CONTINUAS_jah_no_servidor (FILIAL, LINHA, HORA)
+	PRINT 'GEREI TABELA 1'
 
 	-- No servidor eh necessario ter uma tabela com a mesma estrutura
 	-- Usei um linked server (a ser criado em cada estacao de medicao de grau) para comunicar com o servidor.
 	insert into LKSRV_SERVERSQL_BL01.BL01.dbo.MEDICOES_CONTINUAS
-		select *
-		from MEDICOES_CONTINUAS estacao
+		select top 10000 *  -- poucos registros por vez para nao ficar pesado
+	     from MEDICOES_CONTINUAS estacao
 		where not exists (select *
-						from LKSRV_SERVERSQL_BL01.BL01.dbo.MEDICOES_CONTINUAS servidor
+					--	from LKSRV_SERVERSQL_BL01.BL01.dbo.MEDICOES_CONTINUAS servidor
+						from #MEDICOES_CONTINUAS_jah_no_servidor servidor
 						where servidor.FILIAL = estacao.FILIAL COLLATE DATABASE_DEFAULT
 						and servidor.LINHA = estacao.LINHA
 						and servidor.HORA = estacao.HORA)  -- Ainda nao deve existir no servidor
+		  and FILIAL = cast (@filial as int)
+		  and HORA >= @safra
 		
 		-- Somente os ultimos dias para nao ficar muito pesado. Se precisar atualizacao completa, comentariar esta linha.
-		and HORA >= dateadd (day, -5, CURRENT_TIMESTAMP)
+--		and HORA >= dateadd (day, -5, CURRENT_TIMESTAMP)
+	PRINT 'INSERI DADOS 1'
 
 end
