f7f9d17d36c7f861cc78e9ee4573784b04a37b6c
Robertkochbr <robert.koch@novaalianca.coop.br>
2022-03-22 18:39:22 -0300

    View v_wms_item coluna dias_validade

--- Protheus/View v_wms_item.sql
+++ Protheus/View v_wms_item.sql
@@ -3,12 +3,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-
-
-
 ALTER VIEW [dbo].[v_wms_item]
 AS
 -- Cooperativa Vinicola Nova Alianca Ltda
@@ -19,6 +13,7 @@ AS
 -- 07/06/2018 - Robert - Unior para exportar tambem para empresa 02 (almox.02-insumos/embalagens)
 --                     - Busca lastro e camadas no SB1 e nao mais no DC2 e DC3
 -- 05/07/2018 - Robert - Exporta apenas tipo ME para a empresa 2
+-- 22/03/2022 - Robert - Criada coluna dias_validade cfe. chamado 18698 da Full
 --
 
 -- Selecao de itens para a empresa 1 (logistica)
@@ -36,14 +31,15 @@ SELECT RTRIM(B1_COD) AS coditem,
             ELSE 'A'
        END AS situacao,
        ISNULL(SB5.B5_LARG * SB5.B5_COMPR * SB5.B5_ESPESS / 1000000, 0) AS cubagem,	-- estou assumindo que vai ser cadastrado em centimetros.
-	   SB1.B1_VAPLLAS as lastro,
-	   SB1.B1_VAPLCAM as qtdlastro,
+	  SB1.B1_VAPLLAS as lastro,
+	  SB1.B1_VAPLCAM as qtdlastro,
        SB1.B1_VAPLLAS * SB1.B1_VAPLCAM as qtd_palete,
        CASE B1_TIPO
             WHEN 'PA' THEN 'S'
             ELSE 'N'
        END AS inspecao,
-       1 AS empresa  -- AX 01 = empresa 1
+       1 AS empresa,  -- AX 01 = empresa 1
+       SB1.B1_PRVALID as dias_validade
 FROM   SB1010 SB1
        LEFT JOIN SB5010 SB5
             ON  (
@@ -54,8 +50,9 @@ FROM   SB1010 SB1
 WHERE  SB1.D_E_L_E_T_ = ''
        AND B1_FILIAL = '  '
        AND SB1.B1_VAFULLW = 'S'
-	   AND SB1.B1_MSBLQL != '1'
+       AND SB1.B1_MSBLQL != '1'
 
+/* Por enquanto nao vamos implementar no AX 02. Robert, 22/03/2022
 union ALL
 
 -- Selecao de itens para a empresa 2 (almox.02 (embalagens/insumos)
@@ -93,6 +90,6 @@ WHERE  SB1.D_E_L_E_T_ = ''
        AND SB1.B1_TIPO IN ('ME')
        --AND SB1.B1_VAFULLW = 'S'
 	   AND SB1.B1_MSBLQL != '1'
-
+*/
 
 GO
