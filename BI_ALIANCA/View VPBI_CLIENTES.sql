fe8f2ffb56f95144f51165f27d5a6b0e7076b8e9
Robertkochbr <robert.koch@novaalianca.coop.br>
2022-04-22 12:26:46 -0300

    Melhoris para PowerBI

--- BI_ALIANCA/View VPBI_CLIENTES.sql
+++ BI_ALIANCA/View VPBI_CLIENTES.sql
@@ -3,7 +3,16 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER VIEW [dbo].[VPBI_CLIENTES]  -- CLIENTES
-AS 
+AS
+
+-- Cooperativa Vinicola Nova Alianca Ltda
+-- View para buscar dados de clientes, para uso no PowerBI
+-- Autor: Robert Koch
+-- Data:  03/2022
+-- Historico de alteracoes:
+-- 20/04/2022 - Robert - Incluidas colunas A1_VACBASE e A1_VALBASE
+--
+
 SELECT A1_COD
 	, A1_LOJA
 	, A1_NOME
@@ -11,6 +20,8 @@ SELECT A1_COD
 	, A1_REGIAO   -- REGIAO
 	, A1_MESO     -- MESOREGIAO
 	, A1_COD_MUN  -- CODIGO MUNICIPIO IBGE
+	, A1_VACBASE  AS COD_REDE -- CODIGO BASE (REDE)
+	, A1_VALBASE  AS LOJA_REDE -- LOJA BASE (REDE)
 FROM protheus.dbo.SA1010
 WHERE D_E_L_E_T_ = ''
 AND A1_FILIAL = '  '
