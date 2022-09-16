f95efa7af3e0581bdb6b65220e1fb23a94aa7a07
Robertkochbr <robert.koch@novaalianca.coop.br>
2022-09-16 17:23:20 -0300

    View VPBI_REPRESENTANTES melhoras pequenas

--- BI_ALIANCA/View VPBI_REPRESENTANTES.sql
+++ BI_ALIANCA/View VPBI_REPRESENTANTES.sql
@@ -17,13 +17,15 @@ ALTER VIEW [dbo].[VPBI_REPRESENTANTES]  -- REPRESENTANTES
 -- Data:  03/2022
 -- Historico de alteracoes:
 -- 20/04/2022 - Robert - Incluida coluna A3_NREDUZ
---
+-- 16/09/2022 - Robert - Incluidas colunas A3_ATIVO e A3_EST
 
 AS 
 SELECT A3_COD
 	, A3_NOME
 	, A3_VAGEREN  -- GERENTE / SUPERVISOR - VINCULADO A ESTA MESMA TABELA
 	, A3_NREDUZ
+	, A3_ATIVO
+	, A3_EST
 FROM protheus.dbo.SA3010
 WHERE D_E_L_E_T_ = ''
 AND A3_FILIAL = '  '
