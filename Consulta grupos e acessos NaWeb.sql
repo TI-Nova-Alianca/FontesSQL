52b0c270ec0d1e52a74f3fde9eb87c358fe88cb2
Robertkochbr <robert.koch@novaalianca.coop.br>
2023-08-02 16:01:46 -0300

    Consultas de usuarios do NaWeb

--- Consulta grupos e acessos NaWeb.sql
+++ Consulta grupos e acessos NaWeb.sql
@@ -1,6 +1,46 @@
 use naweb
 
+select count (*) from TrnNucAcesso
+--where TrnNucAcessoUsu = 'andressa.brugnera'
+--where TrnNucAcessoPro like '%sa1010%'
+order by TrnNucAcessoDat desc
+
 select * from SecUser where SecUserName = 'pedro.toniolo'
+
+
+-- usuarios sem codigo de pessoa (tenta localizar no Metadados)
+select distinct v.SecUserName
+	, (select max (TrnNucAcessoDat) from TrnNucAcesso where TrnNucAcessoUsu = v.SecUserName) as ult_acesso
+	, META.NOME, META.PESSOA, META.DESCRI_SITUACAO + ' ' + META.DESC_FUNCAO
+from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v
+	left join LKSRV_SIRH.SIRH.dbo.VA_VFUNCIONARIOS META
+		on (upper (META.NOME) COLLATE DATABASE_DEFAULT like upper (substring (v.SecUserName, 1, 4)) + '%'
+		AND upper (META.NOME) COLLATE DATABASE_DEFAULT like '%' + upper (substring (v.SecUserName, len (v.SecUserName)-2, 3))
+		)
+where v.SecUserPessoa is null
+order by v.SecUserName
+
+
+-- funcionalidades que o usuario acessa e em quais perfil elas se encontram
+select TrnNucAcessoUsu, TrnNucAcessoPro, count (*), max (TrnNucAcessoDat)
+	, (select string_agg (SecRoleName + '(' + v.SecRoleDescription + ')', ', ')
+		from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v
+		where v.SecUserName = TrnNucAcesso.TrnNucAcessoUsu
+		and v.SecFunctionalityKey = TrnNucAcesso.TrnNucAcessoPro)
+from TrnNucAcesso
+where TrnNucAcessoPro not in ('WWPBaseObjects.WpnNucEntrar - LOGIN')  -- Telas que todo mundo usa
+and TrnNucAcessoUsu like 'deniandra%'
+group by TrnNucAcessoUsu, TrnNucAcessoPro
+order by count (*) desc
+
+-- acessos de usuarios demitidos (poderia remover esses acessos)
+select distinct v.SecUserId, v.SecUserName, v.SecUserPessoa, v.SecRoleId, v.SecRoleDescription
+	, (select max (TrnNucAcessoDat) from TrnNucAcesso where TrnNucAcessoUsu = v.SecUserName) as ult_acesso
+	,(select DESCRI_SITUACAO + ' ' + DESC_FUNCAO from LKSRV_SIRH.SIRH.dbo.VA_VFUNCIONARIOS META where META.PESSOA = v.SecUserPessoa)
+from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v
+where exists (select * from LKSRV_SIRH.SIRH.dbo.VA_VFUNCIONARIOS META where META.PESSOA = v.SecUserPessoa and META.SITUACAO = 4)
+order by v.SecUserName , SecRoleDescription
+
 
 -- perfis do usuario
 SELECT cast (SUR.SecRoleId as varchar (max)) + ' - ' + SR.SecRoleDescription
@@ -14,19 +54,17 @@ SELECT sur.SecRoleId, sr.SecRoleDescription, sur.SecUserId, su.SecUserName, su.S
 FROM naweb.dbo.SecUserRole sur, naweb.dbo.SecUser su, naweb.dbo.SecRole sr
 where su.SecUserId = sur.SecUserId
 and sr.SecRoleId = sur.SecRoleId
-and su.SecUserName like 'deise%'
---and sur.SecRoleId = 130
+--and su.SecUserName like 'deise%'
+and sur.SecRoleId = 195
 
--- acessos (funcionalidades) liberadas para o perfil
-select r.SecRoleId, r.SecFunctionalityId, f.SecFunctionalityKey, f.SecFunctionalityDescription
-from naweb.dbo.SecFunctionalityRole r, naweb.dbo.SecFunctionality f
-where f.SecFunctionalityId = r.SecFunctionalityId
-and r.SecRoleId = 117
-and not exists (select *
-    from naweb.dbo.SecFunctionalityRole destino
-    where destino.SecRoleId = 196
-    and destino.SecFunctionalityId = r.SecFunctionalityId)
-order by SecFunctionalityKey
+
+-- Funcionalidades que o perfil 1 tem e o perfil 2 nao tem
+select distinct v1.SecRoleId, v1.SecRoleName, v1.SecFunctionalityId, v1.SecFunctionalityKey, v1.SecFunctionalityDescription
+from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v1
+where v1.SecRoleId = 161
+and not exists (select * from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v2
+				where v2.SecRoleId = 228
+				and v2.SecFunctionalityId = v1.SecFunctionalityId)
 
 
 -- Funcionalidades habilitadas em mais de um perfil
@@ -45,5 +83,31 @@ where pf.SecRoleId != 8 -- administrador
 group by pf.SecFunctionalityKey
 having count (*) > 1
 
-select top 100 * from TrnNucAcesso
-order by TrnNucAcessoDat desc
+
+-- quem estah no grupo de manutencao  --, mas nao no de estoques
+select distinct SecUserId, SecUserName, SecUserPessoa, SecRoleName
+	,(select DESCRI_SITUACAO + ' ' + DESC_FUNCAO from LKSRV_SIRH.SIRH.dbo.VA_VFUNCIONARIOS META where META.PESSOA = v.SecUserPessoa)
+from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v
+where v.SecRoleId = 130  -- manutecao
+--and not exists (select *
+--				from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v2
+--				where v2.SecUserId = v.SecUserId
+--				and v2.SecRoleId = 149  -- consultas de estoque
+--				)
+
+
+
+
+-- Para transferir estq:
+-- EstoqueTransferenciaWW
+-- WpnEstTransfEstoque
+-- WpnEstLibTransfEstoque
+-- WWEstoqueTransferencia_Insert
+-- Esta parece que não precisa: WpnEstLibTransfEstoquePrompt
+
+select * from SecFunctionality where SecFunctionalityKey = 'SegurosWW'
+
+
+select * from VA_VUSUARIOS_X_PERFIS_X_FUNCIONALIDADES v
+where lower (v.SecFunctionalityKey) = 'wpnconjuridico'
+
