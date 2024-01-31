be1d7b6121f374b26bb7d3ebe41050e683a67522
Robertkochbr <robert.koch@novaalianca.coop.br>
2024-01-31 07:37:41 -0300

    Melhorias grau com SQL local

--- BL01_estacoes_medicao_grau/Procedura SP_SINCRONIZA_ZZA.sql
+++ BL01_estacoes_medicao_grau/Procedura SP_SINCRONIZA_ZZA.sql
@@ -1,6 +1,6 @@
 USE [BL01]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_SINCRONIZA_ZZA]    Script Date: 18/01/2024 15:07:38 ******/
+/****** Object:  StoredProcedure [dbo].[SP_SINCRONIZA_ZZA]    Script Date: 29/01/2024 14:28:00 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -12,10 +12,11 @@ GO
 --            A intencao eh deixar o programa BL01 (medicao de grau) trabalhando somente
 --            com o SQL Express da estacao, para ganho de velocidade e para evitar que
 --            o programa trave em caso de perda de conexao com o servidor.
--- Data.....: 18/01/2024
+-- Data.....: 24/01/2024
 -- Autor....: Robert Koch
 --
 -- Historico de alteracoes:
+-- 29/01/2024 - Robert - Resincronizava apenas cargas com ZZA_STATUS 1/C. Agora passa a ser 1/C/M
 --
 
 ALTER PROCEDURE [dbo].[SP_SINCRONIZA_ZZA]
@@ -37,6 +38,18 @@ BEGIN
 					  and estacao.ZZA_SAFRA  = servidor.ZZA_SAFRA COLLATE DATABASE_DEFAULT
 					  and estacao.ZZA_CARGA  = servidor.ZZA_CARGA COLLATE DATABASE_DEFAULT)
 
+	-- Se a carga teve alguma alteracao no servidor (foi cancelada ou reenviada), preciso replicar para a estacao.
+	update estacao set ZZA_STATUS = servidor.ZZA_STATUS
+	from BL01.dbo.ZZA010 estacao, LKSRV_SERVERSQL_BL01.protheus.dbo.ZZA010 servidor
+	where estacao.ZZA_FILIAL = @filial
+	  and estacao.ZZA_SAFRA = @safra
+	  and estacao.ZZA_STATUS in ('1', 'C', 'M')
+	  and servidor.ZZA_STATUS != estacao.ZZA_STATUS COLLATE DATABASE_DEFAULT
+	  and servidor.ZZA_FILIAL = estacao.ZZA_FILIAL COLLATE DATABASE_DEFAULT
+	  and servidor.ZZA_SAFRA  = estacao.ZZA_SAFRA  COLLATE DATABASE_DEFAULT
+	  and servidor.ZZA_CARGA  = estacao.ZZA_CARGA  COLLATE DATABASE_DEFAULT
+	  and servidor.ZZA_STATUS in ('1', '3', 'C', 'M')
+
 
 	-- Cria cursor para enviar cada registro da tabela ZZA local para o servidor. Isso por que
 	-- quero gravar em cada registro um campo indicando data e hora em que foi enviado.
@@ -46,6 +59,7 @@ BEGIN
 	declare @ZZACARGA  varchar (4)
 	declare @ZZASTATUS varchar (1)
 	declare @ZZAGRAU float
+	declare @ZZAINIST1 varchar (17)
 	declare @ZZAINIST2 varchar (17)
 	declare @ZZAINIST3 varchar (17)
 
@@ -55,11 +69,11 @@ BEGIN
 		-- quero ter a certeza de reenviar algum registro que possa ter sido perdido em
 		-- execucoes anteriores por falta de conectividade. Quando eu conseguir operar com
 		-- controle de transacoes entre servidores, espero nao precisar mais disso...)
-		select ZZA_FILIAL, ZZA_SAFRA, ZZA_CARGA, ZZA_STATUS, ZZA_GRAU, ZZA_INIST2, ZZA_INIST3
+		select ZZA_FILIAL, ZZA_SAFRA, ZZA_CARGA, ZZA_STATUS, ZZA_GRAU, ZZA_INIST1, ZZA_INIST2, ZZA_INIST3
 		from BL01.dbo.ZZA010 estacao
-		where (ENVIADO_PARA_SERVIDOR is null
-			or ENVIADO_PARA_SERVIDOR > DATEADD (HOUR, -1, getdate()))
-		and exists (select *
+	--	where (ENVIADO_PARA_SERVIDOR is null
+	--		or ENVIADO_PARA_SERVIDOR > DATEADD (HOUR, -1, getdate()))
+		where exists (select *
 						from LKSRV_SERVERSQL_BL01.protheus.dbo.ZZA010 servidor
 						where servidor.ZZA_FILIAL = estacao.ZZA_FILIAL COLLATE DATABASE_DEFAULT
 						  and servidor.ZZA_SAFRA  = estacao.ZZA_SAFRA  COLLATE DATABASE_DEFAULT
@@ -71,28 +85,35 @@ BEGIN
 	OPEN tblcur
 	WHILE 1 = 1
 	BEGIN
-		FETCH tblcur INTO @ZZAFILIAL, @ZZASAFRA, @ZZACARGA, @ZZASTATUS, @ZZAGRAU, @ZZAINIST2, @ZZAINIST3
+		FETCH tblcur INTO @ZZAFILIAL, @ZZASAFRA, @ZZACARGA, @ZZASTATUS, @ZZAGRAU, @ZZAINIST1, @ZZAINIST2, @ZZAINIST3
 
 		IF @@fetch_status <> 0
 			BREAK
 
 		print 'Processando carga ' + @ZZACARGA;
 
-		/* Nao vou aplicar este UPDATE antes de um bom backup. Sacumé...
-		-- LEMBRAR DE VER SE A TRIGGER DOS HORARIOS VAI PERMITIR A CORRENAT SINCRONIZACAO DE HORARIOS!!!!
-		update LKSRV_SERVERSQL_BL01.protheus.dbo.ZZA010 servidor
-			set ZZA_STATUS = @ZZASTATUS, ZZA_GRAU = @ZZAGRAU, ZZA_INIST2 = @ZZAINIST2, ZZA_INIST3 = @ZZAINIST3
-			where servidor.ZZA_FILIAL = @ZZAFILIAL --COLLATE DATABASE_DEFAULT
-			  and servidor.ZZA_SAFRA  = @ZZASAFRA  --COLLATE DATABASE_DEFAULT
-			  and servidor.ZZA_CARGA  = @ZZACARGA  --COLLATE DATABASE_DEFAULT
-		*/
+		-- Faz duas atualizacoes em separado no servidor por que a tabela
+		-- ZZA tem triggers que gravam o horario em que mudou o ZZA_STATUS.
+		-- Soh que preciso que seja levado o horario que tenho na tabela
+		-- da estacao, em vez do SQL do servidor assumir o 'horario atual'.
+		update LKSRV_SERVERSQL_BL01.protheus.dbo.ZZA010
+			set ZZA_STATUS = @ZZASTATUS, ZZA_GRAU = @ZZAGRAU
+			where ZZA_FILIAL = @ZZAFILIAL --COLLATE DATABASE_DEFAULT
+			  and ZZA_SAFRA  = @ZZASAFRA  --COLLATE DATABASE_DEFAULT
+			  and ZZA_CARGA  = @ZZACARGA  --COLLATE DATABASE_DEFAULT
+
+		update LKSRV_SERVERSQL_BL01.protheus.dbo.ZZA010
+			set ZZA_INIST1 = @ZZAINIST1, ZZA_INIST2 = @ZZAINIST2, ZZA_INIST3 = @ZZAINIST3
+			where ZZA_FILIAL = @ZZAFILIAL --COLLATE DATABASE_DEFAULT
+			  and ZZA_SAFRA  = @ZZASAFRA  --COLLATE DATABASE_DEFAULT
+			  and ZZA_CARGA  = @ZZACARGA  --COLLATE DATABASE_DEFAULT
 
 		-- Guarda a hora em que o registro foi enviado para o servidor
-		update BL01.dbo.ZZA010
-			set ENVIADO_PARA_SERVIDOR = getdate()
-			where ZZA_FILIAL = @ZZAFILIAL
-			  and ZZA_SAFRA  = @ZZASAFRA
-			  and ZZA_CARGA  = @ZZACARGA
+	--	update BL01.dbo.ZZA010
+	--		set ENVIADO_PARA_SERVIDOR = getdate()
+	--		where ZZA_FILIAL = @ZZAFILIAL
+	--		  and ZZA_SAFRA  = @ZZASAFRA
+	--		  and ZZA_CARGA  = @ZZACARGA
 
 	END
 	DEALLOCATE tblcur
